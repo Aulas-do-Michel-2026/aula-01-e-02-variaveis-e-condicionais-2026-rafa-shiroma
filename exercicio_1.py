0af96275c51f952a169ec77c0babe745d4adb6fd
rafa-shiroma <rafashiroma@hotmail.com>
2026-04-27 23:37:57 -0300

    Update exercicio_1.py

--- exercicio_1.py
+++ exercicio_1.py
@@ -19,3 +19,12 @@ Dica: Use inputs para receber os dados!
 Lembre de converter ele para o tipo necessário!
 Print na tela com "print"
 """
+
+nota_1 = float(input("Digite a primeira nota: "))
+nota_2 = float(input("Digite a segunda nota: "))
+nota_3 = float(input("DIgite a terceira nota: "))
+
+media = (nota_1 + nota_2 + nota_3) / 3
+
+print(f"A sua média é {media:.2f}")
+
