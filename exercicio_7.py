3a77a4cd7dfadacb5994cd21adca94473ebc88c2
rafa-shiroma <rafashiroma@hotmail.com>
2026-05-02 00:52:18 -0300

    Update exercicio_7.py

--- exercicio_7.py
+++ exercicio_7.py
@@ -88,3 +88,20 @@ Esse exercício está com uma resolução no notebook da aula.
 
 Tente, se não der, olhe lá!
 """
+
+frequencia_populacional = int(input("Digite a frequencia: "))
+gene = input("Digite o gene: ")
+impacto = input("Digite o impacto (ALTO ou BAIXO): ")
+reads = int(input("Digite os reads: "))
+vaf = int(input("Digite o VAF: "))
+
+genes_de_excecao = (gene == 'HFE') or (gene == 'MEFV') or (gene == 'GJB2')
+
+if (reads < 10) or (vaf < 20):
+    print("Não é relevante, deve ser artefato.")
+elif impacto != "ALTO":
+    print("Não é relevante.")
+elif (frequencia_populacional > 5) and not genes_de_excecao:
+    print("Não é relevante.")
+else:
+    print("É relevante.")
